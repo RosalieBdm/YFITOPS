d0b997fb5aec3cc9af6ca6e1afdd74accfcea632
Guillemine Allavena <91621093+GuillemineA@users.noreply.github.com>
2021-10-26 21:52:47 +0200

    Update server.go

--- server.go
+++ server.go
@@ -1,4 +1,5 @@
 package main
+
 //run the server in a cmd with the command "go run server.go portnumber" (8000 works as a portnumber)
 //in another cmd run the user with the commmand "go run user.go portnumber" (and obviously, the same portnumber as the server)
 
@@ -8,36 +9,44 @@ import (
 	"net"
 	"os"
 	"strings"
-	"list"
+	"time"
 )
 
 func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
-		fmt.Println("Please provide host:port.")
+		fmt.Println("Please provide port number")
 		return
 	}
 
-	CONNECT := ":" + arguments[1]
-	c, err := net.Dial("tcp", CONNECT)
+	PORT := ":" + arguments[1]
+	l, err := net.Listen("tcp", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(">> ")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(c, text+"\n")
-
-		message, _ := bufio.NewReader(c).ReadString('\n')
-		fmt.Print("->: " + message)
-		if strings.TrimSpace(string(text)) == "STOP" {
-			fmt.Println("TCP client exiting...")
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
 			return
 		}
-	}
-	//MUSIC := 
+		if strings.TrimSpace(string(netData)) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+			return
+		}
 
+		fmt.Print("-> ", string(netData))
+		t := time.Now()
+		myTime := t.Format(time.RFC3339) + "\n"
+		c.Write([]byte(myTime))
+	}
 }
